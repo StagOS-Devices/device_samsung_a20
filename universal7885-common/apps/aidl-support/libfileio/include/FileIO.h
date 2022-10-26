bbef660d95b031ccc9adb36416602e3fe1210a9a
GKart <gkartyt@gmail.com>
2022-10-26 10:30:16 -0300

    separate a20 device tree from the rest

--- universal7885-common/apps/aidl-support/libfileio/include/FileIO.h
+++
@@ -1,6 +0,0 @@
-namespace FileIO {
-  int readline(const char *path);
-
-  void writeline(const char *path, const std::string& data);
-  void writeline(const char *path, const int data);
-} // namespace FileIO
